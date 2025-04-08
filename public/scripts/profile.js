a83a4d733713dad06b812d215bdc883104c65f23
Krisss46 <kristjan.kristjan@hotmail.com>
2025-04-08 02:58:26 +0100

    8/4/2025 > ...

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,1 +1,40 @@
 document.querySelector("video").playbackRate = 0.7;
+
+
+
+
+
+/* Notes
+function updateEmail() {
+    const emailInput = document.querySelector('#profile-properties input[type="email"]');
+    const email = emailInput.value;
+    if (email) {
+        console.log(`Email updated to: ${email}`);
+        // TODO
+    } else {
+        alert('Please enter a valid email.');
+    }
+}
+
+function updatePassword() {
+    const passwordInput = document.querySelector('#profile-properties input[type="password"]');
+    const password = passwordInput.value;
+    if (password) {
+        console.log('Password updated.');
+        // TODO
+    } else {
+        alert('Please enter a valid password.');
+    }
+}
+
+function updateProfilePicture() {
+    const fileInput = document.querySelector('#profile-properties input[type="file"]');
+    const file = fileInput.files[0];
+    if (file) {
+        console.log(`Profile picture updated: ${file.name}`);
+        // TODO
+    } else {
+        alert('Please select a valid image file.');
+    }
+}
+*/
